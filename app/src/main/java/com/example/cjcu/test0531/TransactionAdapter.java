813a73a56447df261602c542709545c269f78290
wswei0905 <a85090523@gmail.com>
2018-06-06 15:56:38 +0800

    20180606

---
+++ app/src/main/java/com/example/cjcu/test0531/TransactionAdapter.java
@@ -0,0 +1,14 @@
+package com.example.cjcu.test0531;
+
+/**
+ * Created by CJCU on 2018/6/6.
+ */
+
+public class TransactionAdapter {
+
+    private List<Transaction> trans;
+
+    public TransactionAdapter(List<Transaction> trans){
+        this.trans = trans;
+    }
+}
